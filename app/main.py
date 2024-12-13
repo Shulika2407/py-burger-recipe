4548520095120b0bf6cbc279f833ecc24cf2a42a
Liza Shulika <liza.shulika2009@gmail.com>
2024-12-13 22:30:10 +0200

    burger1

--- app/main.py
+++ app/main.py
@@ -1,20 +1,18 @@
 from abc import ABC, abstractmethod
 
-from attr.setters import validate
-
 
 class Validator(ABC):
-    def __set_name__(self, owner, name):
+    def __set_name__(self, owner: type, name: str) -> None:
         self.protected_name = f"_{name}"
 
-    def __get__(self, instance, owner):
+    def __get__(self, instance: object, owner: type) -> None:
         return getattr(instance, self.protected_name)
 
-    def __set__(self, instance, value):
+    def __set__(self, instance: object, value: object) -> None:
         setattr(instance, self.protected_name, value)
 
     @abstractmethod
-    def validate(self, value):
+    def validate(self, value: object) -> None:
         pass
 
 
@@ -23,19 +21,20 @@ class Number(Validator):
         self.min_value = min_value
         self.max_value = max_value
 
-    def validate(self, value):
+    def validate(self, value: int) -> None:
         if not isinstance(value, int):
             raise TypeError("Quantity should be integer")
         if not (self.min_value <= value <= self.max_value):
-            raise ValueError(f"Quantity should not be less than {self.min_value} and greater than {self.max_value}")
-
+            raise ValueError(
+                f"Quantity should not be less than {self.min_value} and greater than {self.max_value}"
+            )
 
 
 class OneOf(Validator):
     def __init__(self, options: list) -> None:
         self.options = options
 
-    def validate(self, value):
+    def validate(self, value: str) -> None:
         if value not in self.options:
             raise ValueError(f"Expected {value} to be one of {self.options}")
 
@@ -47,6 +46,7 @@ class BurgerRecipe:
     cutlets = Number(1, 3)
     eggs = Number(0, 2)
     sauce = OneOf(["ketchup", "mayo", "burger"])
+
     def __init__(self, buns: int,
                  cheese: int,
                  tomatoes: int, cutlets: int,
